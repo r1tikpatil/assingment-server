17361816b3a3eae2de82ec6e0e5e00bc77e2eb0b
Ritik Patil <ritikpatil566@gmail.com>
2022-09-17 23:02:22 +0530

    final

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
